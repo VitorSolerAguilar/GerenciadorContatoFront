5a84ecd88fa166baa07776cac2dd6b68d456f832
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2025-07-01 01:05:33 -0300

    Funcionalidade cadastrar contato adicionada

--- src/app/contato.service.ts
+++ src/app/contato.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Contato } from './contato';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,20 @@ export class ContatoService {
   apiUrl = "http://localhost:3000/contatos";
 
   constructor(private http: HttpClient) { }
+  
+  getAll(): Observable<Contato[]> {
+    return this.http.get<Contato[]>(this.apiUrl);
+  }
 
+  save(contato: Contato): Observable<Contato> {
+    return this.http.post<Contato>(this.apiUrl, contato);
+  }
+
+  delete(contato: Contato): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${contato.id}`);
+  }
+
+  update(contato: Contato): Observable<Contato> {
+    return this.http.put<Contato>(`${this.apiUrl}/${contato.id}`, contato);
+  }
 }
--- src/app/contatos/contatos.component.ts
+++ src/app/contatos/contatos.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contato } from '../contato';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ContatoService } from '../contato.service';
 
 @Component({
   selector: 'app-contatos',
@@ -6,6 +9,49 @@ import { Component } from '@angular/core';
   templateUrl: './contatos.component.html',
   styleUrl: './contatos.component.css'
 })
-export class ContatosComponent {
+export class ContatosComponent implements OnInit {
+  contatos: Contato[] = [];
+  formGroupContato: FormGroup;
+
+  constructor(private service: ContatoService,
+    private formBuilder: FormBuilder
+  ) {
+    this.formGroupContato = formBuilder.group({
+      id: [null],
+      nomeCompleto: [''],
+      telefone: [''],
+      email: [''],
+      endereco: [''],
+      dataNascimento: [''],
+      categoria: [''],
+      apelido: [''],
+      cidade: [''],
+      empresa: [''],
+      notasAdicionais: [''],
+      Contatofavorito: [false]
+    });
+
+  }
+
+  ngOnInit(): void {
+    this.loadContatos();
+  }
+
+  loadContatos() {
+    this.service.getAll().subscribe({
+      next: json => this.contatos = json
+    });
+  }
+
+  save() {
+    this.service.save(this.formGroupContato.value).subscribe(
+      {
+        next: json => {
+          this.contatos.push(json);
+          this.formGroupContato.reset();
+        }
+      }
+    )
+  }
 
 }
